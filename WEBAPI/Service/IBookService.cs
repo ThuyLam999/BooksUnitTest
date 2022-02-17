15f8d5abae7da52b348826429f7e948cfa0f3736
lapham-mdsol <lapham@mdsol.com>
2022-02-17 16:07:20 +0700

    new

---
+++ WEBAPI/Service/IBookService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBAPI.Models;
+
+namespace WEBAPI.Service
+{
+    public interface IBookService
+    {
+        IEnumerable<Book> GetAll();
+        Book Add(Book newBook);
+        Book GetById(Guid id);
+        void Remove(Guid id);
+    }
+}
